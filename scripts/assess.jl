c5762064a6233b843de735e62058aa869994e1eb
Rafael Schouten <rafaelschouten@gmail.com>
2025-02-04 10:34:24 +0100

    updates

--- scripts/assess.jl
+++ scripts/assess.jl
@@ -1,7 +1,78 @@
 using Pkg
-Pkg.instantiate()
+Pkg.activate("ConScapeJobs/")
+# Pkg.instantiate()
 
 import ConScape
 import ConScapeJobs
+using ConScape.Plots
+using JSON3
 
-ConScape.assess(ConScapeJobs.batch_problem(), ConScapeJobs.load_raster())
+rast = ConScapeJobs.load_raster()
+batch_problem = ConScapeJobs.batch_problem()
+
+# summary = ConScape.assess(batch_problem, rast)
+# JSON3.write("summary.json", summary)
+
+struct BatchSummary
+    shape::Tuple{Int,Int}
+    njobs::Int
+    max_windows::Int 
+    max_allocations::Int 
+    window_indices::Vector{Int}
+    window_mask::Vector{Bool}
+    inner_window_jobs::Vector{Int}
+    inner_window_allocations::Vector{Vector{Int}}
+    inner_window_counts::Vector{Int}
+    inner_window_sizes::Vector{Vector{Tuple{Int,Int}}}
+    inner_window_indices::Vector{Vector{Int}}
+    inner_window_masks::Vector{Vector{Bool}}
+end
+
+summary = JSON3.read("summary.json", BatchSummary)
+
+summary.inner_window_allocations
+summary.max_allocations / 1e9
+shape = summary.shape
+sort(maximum.(filter(!isempty, summary.inner_window_allocations)); rev=true)
+
+# Plots
+
+# compute_size = map(summary.inner_window_sizes) do sizes
+#     length(sizes) > 0 ? sum(prod, sizes) : 0
+# end
+# compute_map = reshape(compute_size, shape)
+# job_map = reshape(summary.inner_window_jobs, shape)
+
+# plot(summary.inner_window_jobs)
+# heatmap(rotl90(job_map); title="jobs")
+# heatmap(rotl90(compute_map); title="compute")
+
+
+# Find a job with a wide range of window sizes
+
+using Statistics, StatsBase
+
+# Find the most variable batch
+stds = map(summary.inner_window_sizes) do ws
+    targets = map(last, map(identity, ws))
+    # sum(targets) > 1 ? std(filter(!=(0), targets)) : 0.0
+end
+_, i = findmax(stds)
+
+indices = summary.inner_window_indices[i]
+nwindows = min(length(indices), 2Threads.nthreads())
+window_indices = sample(indices, nwindows; replace=false)
+window_sizes = summary.inner_window_sizes[i]
+summary.inner_window_sizes[i][window_indices]
+job_ranges = ConScape._window_ranges(batch_problem, rast)[i]
+job_rast = rast[job_ranges...]
+window_problem = batch_problem.problem
+
+window_ranges = ConScape._window_ranges(window_problem, job_rast)[window_indices[1]]
+window_rast = ConScape._get_window_with_zeroed_buffer(window_problem, job_rast, window_ranges)
+parent(window_rast.target_qualities)
+plot(window_rast)
+
+@time ConScape.solve(window_problem, job_rast; 
+    window_indices, window_sizes, verbose=true
+)
--- src/problem.jl
+++ src/problem.jl
@@ -2,13 +2,13 @@
 function load_raster()
     # Package test data
 
-    datadir = joinpath(dirname(pathof(ConScape)), "..", "data")
-    target_qualities_path = qualities_path = joinpath(datadir, "qualities_sno_2000.asc")
+    # datadir = joinpath(dirname(pathof(ConScape)), "..", "data")
+    # target_qualities_path = qualities_path = joinpath(datadir, "qualities_sno_2000.asc")
 
     # User data
-    # datadir = "P:/31212200_greenplan/Species_Maps/Pollinators/Connectivity/in_data/"
-    # qualities_path = joinpath(datadir, "TemperateHSM.tif")
-    # target_qualities_path = joinpath(datadir, "TemperateHSM_target.tif")
+    datadir = "/home/NINA.NO/rafael.schouten/Mounts/scratch/tmp_raf/"
+    qualities_path = joinpath(datadir, "TemperateHSM.tif")
+    target_qualities_path = joinpath(datadir, "TemperateHSM_target.tif")
 
     qualities = Raster(qualities_path; lazy=true, missingval=NaN)
     target_qualities = Raster(target_qualities_path; lazy=true, missingval=NaN)
@@ -22,31 +22,38 @@ end
 
 # Problem
 function batch_problem(;
-    nwindows=5,
-    # buffer=200, 
-    # centersize=21, 
-    buffer=10, 
-    centersize=5, 
+    nwindows=50,
+    buffer=200,
+    centersize=21, 
 )
+    ## Define connectivity
+    # Set theta
     θ = 0.5
     α = 60 / 3000
+    # Define a distance transformation
     distance_transformation = x -> exp(-x * α)
     connectivity_measure = ConScape.ExpectedCost(; θ, distance_transformation)
+
+    # Define graph measures
     graph_measures = (;
         ch=ConScape.ConnectedHabitat(),
         betk=ConScape.BetweennessKweighted(),
     )
 
+    ## Specify the problem
     solver = ConScape.MatrixSolver()
     problem = ConScape.Problem(; graph_measures, connectivity_measure, solver)
+    
+    # Specify the windowing pattern
     windowed_problem = ConScape.WindowedProblem(problem; 
         buffer, centersize, threaded=true
     )
+
+    # Specify the batch job windowing
     jobdir = joinpath(dirname(pathof(ConScapeJobs)), "..", "data")
-    joblistpath = joinpath(jobdir, "joblist")
     datapath = joinpath(jobdir, "outputs")
     return ConScape.BatchProblem(windowed_problem; 
-        datapath, joblistpath, nwindows
+        datapath, nwindows
     )
 end
 
