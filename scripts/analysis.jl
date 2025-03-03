0e77ee3ba7787c6b32dd0cf2f350701f643700fa
Rafael Schouten <rafaelschouten@gmail.com>
2025-03-03 12:39:32 +0100

    better test batches

--- scripts/analysis.jl
+++ scripts/analysis.jl
@@ -1,4 +1,6 @@
 nothing
+using Pkg, Revise
+Pkg.activate("ConScapeJobs/")
 using ConScape
 using ConScapeJobs
 using JSON3
@@ -9,5 +11,5 @@ assessment_path = joinpath(ConScapeJobs.datadir, "assessment.json")
 assessment = JSON3.read(assessment_path, ConScape.NestedAssessment) 
 batch_problem = ConScapeJobs.batch_problem()
 rast = ConScapeJobs.load_raster()
-stack = RasterStack(ConScape.batch_paths(batch_problem, rast)[assessment.indices[1]])
+stack1 = RasterStack(ConScape.batch_paths(batch_problem, rast)[assessment.indices[1]])
 plot(stack)
--- scripts/run_job.sh
+++ scripts/run_job.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH --account=nn11055k
 #SBATCH --job-name='batchCS'
-#SBATCH --time=1:30:0
+#SBATCH --time=0:30:0
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=16
 #SBATCH --mem-per-cpu=2G
-#SBATCH --array=0-199 # Start at 0 not 1!
+#SBATCH --array=1-9 # Start at 0 not 1!
 set -o errexit  # Exit the script on any error
 set -o nounset  # Treat any unset variables as an error
 module --quiet purge   # Clear any inherited modules
 module load Julia/1.10.5-linux-x86_64
-julia --threads=4 --project=.. run_job.jl
+julia --threads=auto --project=.. run_job.jl
