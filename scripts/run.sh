274c6ec417513cbd56b3b906cc4b24412984386b
Rafael Schouten <rafaelschouten@gmail.com>
2025-04-03 00:31:00 +0200

    up

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,9 +5,9 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=1500M
-#SBATCH --array=2003-2003 # Start at 0 not 1!
+#SBATCH --array=0-811 # Start at 0 not 1!
 set -o errexit  # Exit the script on any error
 set -o nounset  # Treat any unset variables as an error
 module --quiet purge   # Clear any inherited modules
 module load Julia/1.11.3-linux-x86_64
-julia --threads=4 --project=.. run_job.jl # Lock to threads=4 so threads == cpus
+julia --threads=4 --project=.. run.jl # Lock to threads=4 so threads == cpus
