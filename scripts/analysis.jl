d779285e55239a639da22fd059d2b7a3f058e650
Rafael Schouten <rafaelschouten@gmail.com>
2025-04-04 13:10:39 +0200

    add comments

--- scripts/analysis.jl
+++ scripts/analysis.jl
@@ -6,20 +6,17 @@ using ConScape
 using ConScapeJobs
 using Rasters
 using Plots
-using JSON3
 
-Threads.nthreads()
-
+# Load assessment, problem and rasters
 datadir = ConScapeJobs.path()
-
 assessment =  ConScapeJobs.assessment()
 batch_problem = ConScapeJobs.batch_problem(; threaded=false)
 rast = ConScapeJobs.raster()
 
-paths = ConScape.batch_paths(batch_problem, rast)[assessment.indices]
-
+# Check the assessment and reassess
 assessment
 re = ConScape.reassess(batch_problem, assessment)
+
 
 # Plot the final outputs
 measure_names = keys(ConScape.measures(batch_problem))
@@ -27,6 +24,7 @@ filenames = map(measure_names) do m
     joinpath(datadir, "output_$m.tif")
 end |> NamedTuple{measure_names}
 st = RasterStack(filenames; lazy=true)
+
 # min remove outliers that dominate the colormap
 plot(min.(2000000, st.betk); #[11000:13000, 11000:13000]; 
     title=basename(dirname(datadir)) * " Betweenness K",
--- scripts/mosaic.jl
+++ scripts/mosaic.jl
@@ -13,31 +13,44 @@ pad = ConScapeJobs.settings()["buffer"]::Int
 
 # Get all the filepaths that we made raster files for
 paths = filter(isdir, ConScape.batch_paths(batch_problem, rast)[original_assessment.indices])
+
 # Create lazy RasterStacks for all of them
 stacks = RasterStack.(paths; lazy=true, missingval=NaN)
 GC.gc()
 
-inner_qualities = map(ConScape.measures(batch_problem)) do _
+# Create a zeroed out RasterStack to mosaic into.
+# It will have layer names matching the measures, 
+# each with NaNs where quality is NaN and zeros elsewhere
+# This is all lazy because `rast` is lazy. It liekely would not fit in RAM
+# The view is to unpad the padded rasters and get the original size
+zeroed_qualities = map(ConScape.measures(batch_problem)) do _
     (@view rast.source_qualities[pad+1:end-pad, pad+1:end-pad]) .* 0
 end |> RasterStack
-filename = joinpath(datadir, "output.tif")
-# Start with a zeroed out source_qualities
-dest_filenames = write(filename, inner_qualities; 
-    options="BIGTIFF" => "YES", # In case its larger than 4GB, but also faster somehow
-    chunks=(128, 128), # 128 is faster than 256 and much faster than the default columns
-    missingval=NaN,
-    force=true, 
-)
-dest = RasterStack(dest_filenames; lazy=true, missingval=NaN)
-# Mosaic
+
+# Write the destination rasters
 # Some care is needed here, both in making a file that is easy to use later
 # and in making the mosaic faster. If we chunk it is much faster because
-# Each mosaic only writes to a small area each time, rather than whole columns.
+# Each mosaic only writes to a small area rather than whole columns.
 # This will also make reading subsets faster for users of the data.
+filename = joinpath(datadir, "output.tif")
+dest_filenames = write(filename, zeroed_qualities; 
+    options="BIGTIFF" => "YES", # In case its larger than 4GB, but also faster somehow
+    chunks=(128, 128), # 128 is faster than 256 and much faster than the default columns
+    missingval=NaN, # Make sure the missing value is NaN
+    force=true, # Overwrite if it already exists
+)
+
+# Read the new rasters lazily to `dest`
+dest = RasterStack(dest_filenames; 
+    lazy=true, # Only load metadata, data stays on disk
+    missingval=NaN # Make sure the missing value is NaN, not `missing`
+)
+
+# Mosaic all stacks into `dest`
 @time open(dest; write=true) do D
     mosaic!(sum, D, stacks; 
         read=true, # Read every region from disk before the mosaic
-        gc=50, # Garbage collect every 50 rasters to control memory
+        gc=50, # Garbage collect every 50th raster to control memory use
     );
 end
 
